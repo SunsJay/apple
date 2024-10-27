849f222e1739535cf279ffac1163b7d6cbcf03e8
sunsjay <sunsjay0806@gmail.com>
2024-10-27 22:42:51 +0800

    feat: :sparkles: page 1 example

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,30 @@ const App: React.FC = () => {
     const [vmExePath, setVmExePath] = useState("C:\\Program Files (x86)\\VMware\\VMware Workstation\\vmrun.exe");
     const [masterMacPath, setMasterMacPath] = useState("D:\\mupan")
     const [sonMacPath, setSonMacPath] = useState("D:\\zipan")
+    // 定义页面1的组件
+    const Page1 = () => {
+        return (
+            <div>
+                <h1>This is Page 1</h1>
+                <p>Welcome to Page 1</p>
+            </div>
+        );
+    };
 
+    const handleButtonClick = (page: string) => {
+        // 实现页面跳转的逻辑，这里可以根据需要进行不同页面的跳转
+        console.log(`Navigating to ${page}`)
+        Page1()
+    };
     return (
         <main className="container">
             <h1>虚拟机管理系统</h1>
+
+            <div>
+                <button onClick={() => handleButtonClick('page1')}>Go to Page 1</button>
+                <button onClick={() => handleButtonClick('page2')}>Go to Page 2</button>
+                <button onClick={() => handleButtonClick('page3')}>Go to Page 3</button>
+            </div>
 
             <form className="row file-selectors-container">
                 <FileSelector defaultPath={vmExePath} onSelect={(path: string) => setVmExePath(path)}
