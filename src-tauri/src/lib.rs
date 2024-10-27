3df8ffeb408cc5d3a07e6df9d8c7e71a96418dc7
sunsjay <sunsjay0806@gmail.com>
2024-10-27 18:26:12 +0800

    fix: :bug: 修复选择文件值不更新

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -17,6 +17,8 @@ pub fn run() {
         .plugin(tauri_plugin_log::Builder::new().build())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_shell::init())
+        .plugin(tauri_plugin_persisted_scope::init())
+
         .invoke_handler(tauri::generate_handler![greet])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
