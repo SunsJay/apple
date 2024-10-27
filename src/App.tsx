4becfb48465d2369a41f8f640a3aaa765450ed2a
sunsjay <sunsjay0806@gmail.com>
2024-10-27 17:49:10 +0800

    style: :lipstick: css

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,17 @@
 import {useState} from "react";
-import reactLogo from "./assets/react.svg";
-import {invoke} from "@tauri-apps/api/core";
+// import reactLogo from "./assets/react.svg";
+// import {invoke} from "@tauri-apps/api/core";
 import {open} from '@tauri-apps/plugin-dialog';
 import "./App.css";
 
 function App() {
-    const [greetMsg, setGreetMsg] = useState("");
-    const [name, setName] = useState("");
+
     const [vmExePath, setVmExePath] = useState("C:\\Program Files (x86)\\VMware\\VMware Workstation\\vmrun.exe");
 
-    async function greet() {
-        // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-        setGreetMsg(await invoke("greet", {name}));
-    }
+    // async function greet() {
+    //     // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+    //     setGreetMsg(await invoke("greet", {name}));
+    // }
 
     async function open_folder() {
         const folder = await open(
@@ -22,6 +21,7 @@ function App() {
             }
         );
 
+        // @ts-ignore
         setVmExePath(folder)
     }
 
