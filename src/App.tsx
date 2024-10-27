b04872caef107ffc2f96b71a2e7e6fce9b4c3f24
sunsjay <sunsjay0806@gmail.com>
2024-10-27 19:37:22 +0800

    feat: :sparkles: 新增os info 功能

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,21 @@ import {useEffect, useState} from "react";
 // import {invoke} from "@tauri-apps/api/core";
 import {open} from '@tauri-apps/plugin-dialog';
 import "./App.css";
+import {platform} from "@tauri-apps/plugin-os";
 
 function App() {
 
     const [vmExePath, setVmExePath] = useState("C:\\Program Files (x86)\\VMware\\VMware Workstation\\vmrun.exe");
+    const [osInfo, setOsInfo] = useState("");
+
+    useEffect(() => {
+        os_info(); // 在页面更新时运行os_info函数
+    }, []);
+    let os_info = async () => {
+        const currentPlatform = await platform();
+        console.log(currentPlatform);
+        setOsInfo(currentPlatform)
+    }
 
     async function choose_vmexe_path() {
         const path = await open(
@@ -25,11 +36,8 @@ function App() {
         <main className="container">
             <h1>虚拟机管理系统</h1>
 
-
-            <form
-                className="row"
-
-            >
+            <p>{osInfo}</p>
+            <form className="row">
                 <input
                     id="choose-vmexe-path"
                     onChange={(e) => setVmExePath(e.currentTarget.value)}
@@ -38,6 +46,8 @@ function App() {
                 />
                 <button type="button" onClick={choose_vmexe_path}>VMRUN路径</button>
             </form>
+
+
         </main>
     );
 }
