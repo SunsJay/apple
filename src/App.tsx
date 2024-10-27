0502dea1e3a76bc4f59e1c698dbb30909a271be7
sunsjay <sunsjay0806@gmail.com>
2024-10-28 00:20:10 +0800

    feat: :sparkles: 新增虚拟机列表界面

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, {useState} from "react";
 import "./App.css";
 import {LocalConfigPage, ScriptConfigPage} from "./components/ConfigPages";
 import ControlPage from "./components/ControlPage.tsx";
+import MacListsPage from "./components/MacListsPage.tsx";
 
 const App: React.FC = () => {
     const [currentPage, setCurrentPage] = useState('控制台');
@@ -25,6 +26,7 @@ const App: React.FC = () => {
                 <button onClick={() => handlePageChange('控制台')}>控制台</button>
                 <button onClick={() => handlePageChange('本地配置')}>本地配置</button>
                 <button onClick={() => handlePageChange('脚本配置')}>脚本配置</button>
+                <button onClick={() => handlePageChange('虚拟机列表')}>虚拟机列表</button>
             </header>
 
 
@@ -42,6 +44,7 @@ const App: React.FC = () => {
                     />
                 )}
                 {currentPage === '脚本配置' && <ScriptConfigPage/>}
+                {currentPage === '虚拟机列表' && <MacListsPage/>}
             </div>
 
 
---
+++ src/components/MacListsPage.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const MacListsPage: React.FC = () => {
+
+    return (
+        <div>
+            <p>虚拟机信息列表</p>
+
+        </div>
+    );
+}
+
+export default MacListsPage;
