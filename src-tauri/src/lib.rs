2ce92708503f02c23c429bf18c72d3baf5e89f89
sunsjay <sunsjay0806@gmail.com>
2024-10-28 01:48:26 +0800

    feat: :sparkles: 新增store

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,20 +1,10 @@
-use std::env;
-
-use dotenv::dotenv;
-
 // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
-    dotenv().ok();
-    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
-    println!("{}", database_url);
     tauri::Builder::default()
-        .plugin(tauri_plugin_store::Builder::new().build())
+        .plugin(tauri_plugin_store::Builder::default().build())
         .plugin(tauri_plugin_clipboard_manager::init())
         // .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_sql::Builder::new().build())
@@ -27,7 +17,7 @@ pub fn run() {
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_persisted_scope::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,31 @@
 import React, {useState} from "react";
 import "./App.css";
 import {LocalConfigPage, ScriptConfigPage} from "./components/ConfigPages";
-import ControlPage from "./components/ControlPage.tsx";
-import MacListsPage from "./components/MacListsPage.tsx";
+import ControlPage from "./components/ControlPage";
+import MacListsPage from "./components/MacListsPage";
+import {store} from "./utils/store.ts";
 
 const App: React.FC = () => {
+
+    store();
     const [currentPage, setCurrentPage] = useState('控制台');
-
-
     const [vmExePath, setVmExePath] = useState("C:\\Program Files (x86)\\VMware\\VMware Workstation\\vmrun.exe");
-    const [masterMacPath, setMasterMacPath] = useState("D:\\mupan")
-    const [sonMacPath, setSonMacPath] = useState("D:\\zipan")
-    // 定义页面1的组件
+    const [masterMacPath, setMasterMacPath] = useState("D:\\mupan");
+    const [sonMacPath, setSonMacPath] = useState("D:\\zipan");
 
     const handlePageChange = (page: string) => {
         setCurrentPage(page);
     };
 
 
-    // @ts-ignore
     return (
         <main className="container">
-
             <header className="button-container">
                 <button onClick={() => handlePageChange('控制台')}>控制台</button>
                 <button onClick={() => handlePageChange('本地配置')}>本地配置</button>
                 <button onClick={() => handlePageChange('脚本配置')}>脚本配置</button>
                 <button onClick={() => handlePageChange('虚拟机列表')}>虚拟机列表</button>
             </header>
-
 
             <div>
                 {currentPage === '控制台' && <ControlPage/>}
@@ -46,10 +43,8 @@ const App: React.FC = () => {
                 {currentPage === '脚本配置' && <ScriptConfigPage/>}
                 {currentPage === '虚拟机列表' && <MacListsPage/>}
             </div>
-
-
         </main>
     );
-}
+};
 
 export default App;
