394b659fff7be8839936732fd19357657d8e434d
sunsjay <sunsjay0806@gmail.com>
2024-10-29 23:40:35 +0800

    style: :lipstick: 修改样式

--- src/components/ControlPage.tsx
+++ src/components/ControlPage.tsx
@@ -6,28 +6,34 @@ const ControlPage: React.FC = () => {
 
     const statStyle: React.CSSProperties = {
         position: 'absolute',
-        top: '-30px',
+        top: '-30%',
+        left: '10%',
+        transform: 'translate(-50%, -50%)',
         backgroundColor: '#f6f6f6',
         padding: '5px',
-        left: '10px', // 调整左边距以适应框的宽度
+    };
+
+    const containerStyle: React.CSSProperties = {
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        border: '1px solid black',
+        padding: '10px',
+        marginTop: '10px',
+        position: 'relative',
+        minHeight: '50px',
+        boxSizing: 'border-box',
     };
 
     const inlineStyle: React.CSSProperties = {
         display: 'inline-block',
-        marginRight: '20px', // 设置间距
+        margin: '0 20px',
     };
 
     return (
         <div>
             <p>控制台</p>
-            <div style={{
-                border: '1px solid black',
-                padding: '10px',
-                marginTop: '10px',
-                position: 'relative',
-                minHeight: '50px',
-                boxSizing: 'border-box',
-            }}>
+            <div style={containerStyle}>
                 <p style={statStyle}>虚拟机数量统计：</p>
                 <p style={inlineStyle}>总数： {totalNumbers}</p>
                 <p style={inlineStyle}>可用： {totalNumbers}</p>
@@ -35,14 +41,7 @@ const ControlPage: React.FC = () => {
                 <p style={inlineStyle}>已失效： {totalNumbers}</p>
             </div>
 
-            <div style={{
-                border: '1px solid black',
-                padding: '10px',
-                marginTop: '10px',
-                position: 'relative',
-                minHeight: '50px',
-                boxSizing: 'border-box',
-            }}>
+            <div style={containerStyle}>
                 <p style={statStyle}>运行实例状态：</p>
                 <p style={inlineStyle}>开机任务数： {totalNumbers}</p>
                 <p style={inlineStyle}>执行任务中： {totalNumbers}</p>
