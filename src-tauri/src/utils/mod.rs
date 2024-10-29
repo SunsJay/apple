1da3482705febc784d8535dd641044a2e5738491
sunsjay <sunsjay0806@gmail.com>
2024-10-29 17:44:44 +0800

    feat: :sparkles: 新增env定位

--- src-tauri/src/utils/mod.rs
+++ src-tauri/src/utils/mod.rs
@@ -1,7 +1,15 @@
 #[tauri::command]
 pub fn get_env_var(key: &str) -> String {
-    match dotenv::var(key) {
-        Ok(v) => { v }
-        Err(_) => { "".to_string() }
+    let env_file_path = Path::new(".").join(".env");
+    match dotenv(env_file_path) {
+        Ok(_) => {
+            match env::var(key) {
+                Ok(val) => val,
+                Err(_) => "".to_string()
+            }
+        }
+        Err(_) => {
+            "".to_string()
+        }
     }
 }
