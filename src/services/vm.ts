d49a5a7e9bc3c9cd52ffd7f499067815c8e11777
sunsjay <sunsjay0806@gmail.com>
2024-10-30 15:08:27 +0800

    feat: :sparkles: add log

--- src/services/vm.ts
+++ src/services/vm.ts
@@ -3,6 +3,7 @@ import {invoke} from "@tauri-apps/api/core";
 export const vmrunList = async (vmExePath: string) => {
     try {
         // @ts-ignore
+        console.log(vmExePath);
         const res = await invoke('vmrun_list', vmExePath);
         console.log(res)
         return res
